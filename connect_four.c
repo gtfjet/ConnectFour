b8e77c2dcdbe7b0674f0330df298969fca11da5d
dave <gtfjet@gmail.com>
2016-03-13 18:16:25 -0400

    added loop, win/loss stats, window sizing

--- connect_four.c
+++ connect_four.c
@@ -5,27 +5,44 @@
 
 int map[12][13];
 int height[7];
+int win, loss, draw;
 
 void draw_board() {
 	int i, j, x;
 	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+	SMALL_RECT size = {0,0,17,17};
+	COORD coord = {18,18};
 	CONSOLE_FONT_INFOEX info = {0};
-    info.cbSize       = sizeof(info);
+	info.cbSize = sizeof(info);
     info.dwFontSize.Y = 36;
-    info.FontWeight   = FW_NORMAL;
+    info.FontWeight = FW_NORMAL;
     wcscpy(info.FaceName, L"Lucida Console");
+	SetConsoleWindowInfo(h, TRUE, &size);
+	SetConsoleScreenBufferSize(h, coord);
     SetCurrentConsoleFontEx(h, FALSE, &info);
-	SetConsoleTextAttribute(h,240);//10);
+	SetConsoleTextAttribute(h,240);
 	system("cls");
-	printf("~~Connect Four~~\n\n\n");
+	
+	printf("~~Connect Four~~\n\n");
+	SetConsoleTextAttribute(h,249);
+	printf("%d",win);
+	SetConsoleTextAttribute(h,240);
+	printf("/");
+	SetConsoleTextAttribute(h,252);
+	printf("%d",loss);	
+	SetConsoleTextAttribute(h,240);
+	printf("/");
+	SetConsoleTextAttribute(h,250);
+	printf("%d\n\n",draw);
+	SetConsoleTextAttribute(h,240);
+		
 	for (i=0; i<6; i++) {
 		for (j=0; j<7; j++) {
 			x = map[5-i+3][j+3];
 			printf("|");
-			//SetConsoleTextAttribute(h,x*(12-3*x));
 			SetConsoleTextAttribute(h,4.5*x*x-10.5*x+255);
 			printf("%c",254);
-			SetConsoleTextAttribute(h,240);//10);
+			SetConsoleTextAttribute(h,240);
 		}
 		printf("|\n");
 	}
@@ -151,44 +168,78 @@ int move_computer() {
 	return c;
 }
 
-void main() {
-	int i, c, u;
-	srand(time(NULL));
-	draw_board();
-	printf("\n");
-	for (i=0; i<21; i++) {
+int move_player() {
+	int u;
+	printf("Player: ");
+	scanf("%d", &u);
+	getchar();
+	while (u<1 || u>7 || height[u-1]==6) {
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),247);
+		printf("bad input value\n");
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),240);
 		printf("Player: ");
 		scanf("%d", &u);
-		while (u<1 || u>7 || height[u-1]==6) {
-			printf("bad input value\n");
-			printf("Player: ");
-			scanf("%d", &u);
-		}
-		u--;  // switch to zero based
-		map[height[u]+3][u+3] = 1;
-		height[u]++;
-		draw_board();
-		if (check_win(u, 1)) {
-			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),249);
-			printf("Player wins!\n");
-			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),240);
-			system("pause");
-			return;
-		}
-		c = move_computer();
-		draw_board();
-		printf("Computer: %d\n",c+1);
-		if (check_win(c, 2)) {
-			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),252);
-			printf("Computer wins!\n");
-			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),240);
-			system("pause");
-			return;
-		}
+		getchar();
 	}
-	printf("Draw!\n");
-	system("pause");
-	return;
+	u--;  // switch to zero based
+	map[height[u]+3][u+3] = 1;
+	height[u]++;
+	return u;
+}
+
+void main() {
+	int i, c, u, n;
+	srand(time(NULL));
+
+	while(1) {
+		memset(map, 0, sizeof map);
+		memset(height, 0, sizeof height);
+		draw_board();
+		printf("\n");
+		
+		for (i=0; i<21; i++) {
+			if (n%2 == 0) {
+				u = move_player();
+				draw_board();
+				if (check_win(u, 1)) {
+					win++;
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),249);
+					printf("Player wins!\n");
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),240);
+					break;
+				}
+			}
+			c = move_computer();
+			draw_board();
+			printf("Computer: %d\n",c+1);
+			if (check_win(c, 2)) {
+				loss++;
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),252);
+				printf("Computer wins!\n");
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),240);
+				break;
+			}
+			if (n%2 == 1) {
+				u = move_player();
+				draw_board();
+				if (check_win(u, 1)) {
+					win++;
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),249);
+					printf("Player wins!\n");
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),240);
+					break;
+				}
+			}
+			if (i==20) {
+				draw++;
+				(GetStdHandle(STD_OUTPUT_HANDLE),250);
+				printf("Draw!\n");
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),240);
+			}
+		}
+		n++;
+		getchar();
+	}
 }
 
 
